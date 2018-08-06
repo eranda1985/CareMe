4153d6a7301c65da96e6caa0a1d424166cc20cf0
Eranda Galhenage <eranda.galhenage@unisa.edu.au>
2018-08-06 11:42:20 +0930

    Code refactoring in Api Gateway.

--- ApiGateway/Program.cs
+++ ApiGateway/Program.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.IO;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
 
 namespace ApiGateway
 {
@@ -20,6 +14,7 @@ namespace ApiGateway
     public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
         WebHost.CreateDefaultBuilder(args)
       .UseKestrel()
+      .UseIISIntegration()
       .UseContentRoot(Directory.GetCurrentDirectory())
       .UseStartup<Startup>();
   }
