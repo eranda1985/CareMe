93f2bffe2be14792b551bc6673194ae27dd2efdf
eranda <eranda1985@yahoo.com>
2018-11-15 23:13:31 +1030

    Added Vehicle.API

---
+++ Vehicle.API/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace Vehicle.API
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			CreateWebHostBuilder(args).Build().Run();
+		}
+
+		public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+				WebHost.CreateDefaultBuilder(args)
+			.UseKestrel()
+						.UseStartup<Startup>()
+			.ConfigureLogging((hostingCtx, loggerBuilder)=> 
+			{
+				loggerBuilder.AddLog4Net();
+				loggerBuilder.SetMinimumLevel(LogLevel.Debug);
+			});
+	}
+}
