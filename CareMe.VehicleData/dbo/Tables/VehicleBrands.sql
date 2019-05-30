53cb4266d9460196be86b3a7d7c12b68760fe8cf
Eranda Galhenage <eranda.galhenage@unisa.edu.au>
2019-05-30 12:56:05 +0930

    Updates to database project.

---
+++ CareMe.VehicleData/dbo/Tables/VehicleBrands.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[VehicleBrands] (
+    [Id]    BIGINT        IDENTITY (1, 1) NOT NULL,
+    [Brand] NVARCHAR (50) NULL
+);
+
---
+++ CareMe.VehicleData/dbo/Tables/VehicleModels.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[VehicleModels] (
+    [Id]      BIGINT         IDENTITY (1, 1) NOT NULL,
+    [BrandId] BIGINT         NULL,
+    [Model]   NVARCHAR (100) NULL
+);
+
