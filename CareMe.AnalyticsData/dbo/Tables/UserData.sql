f14cc92297fd44cf877cc138f12f2e215b32aa75
Eranda Galhenage <eranda.galhenage@unisa.edu.au>
2019-06-03 09:56:29 +0930

    Updates to Analytics DB project.

---
+++ CareMe.AnalyticsData/dbo/Tables/UserData.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[UserData] (
+    [Id]        BIGINT        NOT NULL,
+    [SecretKey] NVARCHAR (70) NULL,
+    [UserName]  NVARCHAR (50) NULL,
+    CONSTRAINT [PK_UserData] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
+
+
---
+++ CareMe.AnalyticsData/dbo/Tables/VehicleDetails.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[VehicleDetails] (
+    [Id]           BIGINT        NOT NULL,
+    [VehicleId]    BIGINT        NOT NULL,
+    [Rego]         NVARCHAR (50) NULL,
+    [LastOdometer] FLOAT (53)    NULL,
+    [LastUpdated]  DATETIME      NULL,
+    CONSTRAINT [PK_VehicleDetails] PRIMARY KEY CLUSTERED ([VehicleId] ASC)
+);
+
+
+
