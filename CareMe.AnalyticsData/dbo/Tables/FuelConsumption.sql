2dc148366140b5f66a24373c4cedae30440c8d3c
Eranda Galhenage <eranda.galhenage@unisa.edu.au>
2019-06-03 09:59:27 +0930

    Table added in Analytics DB.

---
+++ CareMe.AnalyticsData/dbo/Tables/FuelConsumption.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[FuelConsumption] (
+    [Id]        BIGINT     NOT NULL,
+    [VehicleId] BIGINT     NULL,
+    [Litres]    FLOAT (53) NULL,
+    [Date]      DATETIME   NULL,
+    [Amount]    FLOAT (53) NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
