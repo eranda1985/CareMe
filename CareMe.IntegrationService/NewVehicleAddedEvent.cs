7040ec51fbd87d24b0d8c6cf859d1101fb1482dc
Eranda Galhenage <eranda.galhenage@unisa.edu.au>
2019-06-04 10:09:36 +0930

    EventClass added.

---
+++ CareMe.IntegrationService/NewVehicleAddedEvent.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CareMe.IntegrationService
+{
+	public class NewVehicleAddedEvent: IntegrationEvent
+	{
+		public long VehicleId { get; set; }
+		public string Rego { get; set; }
+		public double LastODOMeter { get; set; }
+		public DateTime LastUpdated { get; set; }
+	}
+}
