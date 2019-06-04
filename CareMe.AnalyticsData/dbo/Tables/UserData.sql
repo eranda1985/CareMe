4fa26702b601983866d4afe4215fa80fe0c4b738
Eranda Galhenage <eranda.galhenage@unisa.edu.au>
2019-06-04 16:24:25 +0930

    Updates to tables in Analytics DB.

--- CareMe.AnalyticsData/dbo/Tables/UserData.sql
+++ CareMe.AnalyticsData/dbo/Tables/UserData.sql
@@ -1,9 +1,11 @@
 ﻿CREATE TABLE [dbo].[UserData] (
-    [Id]        BIGINT        NOT NULL,
+    [Id]        BIGINT        IDENTITY (1, 1) NOT NULL,
     [SecretKey] NVARCHAR (70) NULL,
     [UserName]  NVARCHAR (50) NULL,
     CONSTRAINT [PK_UserData] PRIMARY KEY CLUSTERED ([Id] ASC)
 );
+
+
 
 
 
