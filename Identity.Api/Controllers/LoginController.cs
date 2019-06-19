139819afc4a9f8067c9c5a9893cf2b60834faa87
Eranda Galhenage <eranda.galhenage@unisa.edu.au>
2019-06-19 11:34:49 +0930

    Added User controller.

--- Identity.Api/Controllers/LoginController.cs
+++ Identity.Api/Controllers/LoginController.cs
@@ -74,34 +74,5 @@ namespace Identity.Api.Controllers
 
             return Ok(res);
         }
-
-        [HttpGet]
-        [Route("profile/{username}")]
-        [ProducesResponseType(200, Type = typeof(List<UserProfileDto>))]
-        [ProducesResponseType(401)]
-        [MapToApiVersion("1.0")]
-        public async Task<IActionResult> GetProfile([FromRoute] string username)
-        {
-            _logger.LogDebug("Entering user profile resource");
-
-            var res = await ((UserService)_userService).GetUserProfile(username);
-
-            return Ok(res);
-        }
-
-        [HttpPost]
-        [Route("profile")]
-        [ProducesResponseType(200, Type = typeof(bool))]
-        [ProducesResponseType(401)]
-        [MapToApiVersion("1.0")]
-        public async Task<IActionResult> AfddUserProfile([FromBody] UserProfileRequest requestBody)
-        {
-            _logger.LogDebug("Entering add user profile endpoint");
-
-            var res = await ((UserService)_userService)
-                .AddUserProfile(requestBody.Username, requestBody.First, requestBody.Last, requestBody.Mobile);
-
-            return Ok(res);
-        }
     }
 }
