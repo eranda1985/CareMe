009b3e0d5ae103d8beb1fdfedb330f47821cb6f0
Eranda Galhenage <eranda.galhenage@unisa.edu.au>
2019-07-04 13:30:47 +0930

    Work in progres with updating odometer when added a new fuel record.

---
+++ Vehicle.API/IntegrationEventHandlers/FuelRecordAddedEventHandler.cs
@@ -0,0 +1,16 @@
+﻿using CareMe.IntegrationService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Vehicle.API.IntegrationEventHandlers
+{
+	public class FuelRecordAddedEventHandler : IIntegrationEventHandler
+	{
+		public Task HandleEvent(IntegrationEvent @event)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
