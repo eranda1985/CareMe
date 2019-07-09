78b258af01b014af996d688c8ab352b089c332f4
Eranda Galhenage <eranda.galhenage@unisa.edu.au>
2019-07-09 12:34:05 +0930

    Partial work

--- Identity.Model/Factory/SmtpClient.cs
+++ Identity.Model/Factory/SmtpClient.cs
@@ -23,7 +23,7 @@ namespace Identity.Model.Factory
 		/// <summary>
 		/// Sends an e-mail. 
 		/// </summary>
-		/// <param name="args"></param>
+		/// <param name='args'></param>
 		public void SendRequest(params object[] args)
 		{
 			_exceptionService.Throw(() => Validator.CheckArgsLength(args, 2));
@@ -40,13 +40,26 @@ namespace Identity.Model.Factory
 				EnableSsl = true
 			};
 
+			//var img = new LinkedResource()
+			var mailBody = string.Format(HtmlBody, signUpCode, "\u00a9");
+
 			MailMessage mail = new MailMessage(emailObj.FromAddress, emailObj.ToAddress)
 			{
 				Subject = "New Sign-up Request",
-				Body = string.Format("Please use the following code to sign-up: {0}", signUpCode)
+				Body = mailBody
 			};
+
+			mail.IsBodyHtml = true;
 
 			smtpClient.Send(mail);
 		}
+
+		string HtmlBody => @"<body style='margin:0px; padding:0px;' bgcolor='#efefef'>
+<table align='center' border='0' cellspacing='0' cellpadding='0' ><tr><td valign='top' align='center'><img src='smiley.gif' alt='Smiley face' /><br /><br /><table width='100%' cellspacing='0' cellpadding='0' border='0' align='center'><tbody><tr><td style='font-family:'Open Sans', Arial, sans-serif; font-size:12px; line-height:15px; color:#0d1121;' valign='top' align='center'>
+<h2>New sign up request for Cargenik App{1}</h2>
+<table align = 'center' border='0' cellspacing='0' cellpadding='0'>
+<tr><td align = 'center' valign='top' style='font-family:'Open Sans', Arial, sans-serif; font-size:16px; line-height:30px; color:#000000;'>
+<h3>Please use  the following code to sign up: <b> {0} </b></h3>
+</td></tr><tr><td height = '15' style='font-size:0px; line-height:0px; height:15px;'>&nbsp;</td></tr></table></td></tr></tbody></table></td></tr></table></body>";
 	}
 }
